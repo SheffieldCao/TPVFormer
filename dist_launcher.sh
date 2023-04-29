e225b3cf9df3389ed9d0e318b535b67961ff949b
SheffieldCao <1751899@tongji.edu.cn>
2023-04-29 15:24:43 +0000

    [Add] mini

---
+++ dist_launcher.sh
@@ -0,0 +1,17 @@
+CONFIG=$1
+GPUS=$2
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+python -m torch.distributed.launch \
+    -m torch.utils.collect_env \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    ./dist_train.py \
+    $CONFIG \
+    --launcher pytorch ${@:3}
--- launcher.sh
+++ launcher.sh
@@ -4,4 +4,5 @@ WORKDIR=$2
 
 shift 2
 
+CUDA_VISIBILE_DEVICES="1" \
 python train.py --py-config $CONFIG --work-dir $WORKDIR "$@"
