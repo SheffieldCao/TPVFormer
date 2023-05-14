a5ef44d41999f3592b62877cadd34622624af4d9
SheffieldCao <1751899@tongji.edu.cn>
2023-05-14 16:08:29 +0000

    Replace tools scripts

--- launcher.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-CONFIG=$1
-WORKDIR=$2
-
-shift 2
-
-CUDA_VISIBILE_DEVICES="1" python train.py --py-config $CONFIG --work-dir $WORKDIR "$@"
--- vis.sh
+++
@@ -1,6 +0,0 @@
-# vis frames by my model
-python visualization/vis_frame.py --py-config config/tpv04_occupancy-vox_4e-1-dim_128-base.py \
-                                --work-dir work_dirs/tpv04_occupancy-vox_4e-1-dim_128-base \
-                                --ckpt-path work_dirs/tpv04_occupancy-vox_4e-1-dim_128-base/epoch_16.pth \
-                                --save-path work_dirs/tpv04_occupancy-vox_4e-1-dim_128-base/vis \
-                                --frame-idx 0 1 2 3 4 5 6
